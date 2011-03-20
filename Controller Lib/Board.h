331993480e17b8e1f95f9ba346f1622307dd35a3
com_kieffer <comkieffer.me@gmail.com>
2011-03-20 20:42:31 +0100

    Added definitions for buildSimpleLine(), buildRepeatLIne() and getCommandFile(). Added to lists for simple and repeat lines.
    Made some cosmetic changes (comments)

--- Controller Lib/Board.h
+++ Controller Lib/Board.h
@@ -18,12 +18,18 @@ class Board
         int getNumberOfRelays();
 
 //NOTE : relay state changes when board is used in tethered mode
-        std::string getRelayState();
-        int         getRelayState(int relay);
+        //std::string getRelayState();
+        int getRelayState(int relay);
+#ifdef SHELL_MODE
+        void lua_RelayState();
+#endif
 
-        int getBoardTime();
+        int getTime();
 
         bool isConnected();
+
+        bool toggleRelay(int relay);
+        bool commandRelay(int relay, int action);
 
     private:
         void connectToBoard();
@@ -37,11 +43,13 @@ class Board
 
         int _relays;
         time_t _board_time;
-        std::string _board_type, _board_rev, _board_program, _current_ratings;
+        std::string _board_type, _board_rev, _board_program, _current_ratings, _relay_state;
 };
 
 namespace RELAY_STATE {
     enum {OFF, ON};
 };
+
+
 
 #endif // BOARD_H
