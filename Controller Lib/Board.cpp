49085eb26532cb683f31dda82b29765a54a849c0
com_kieffer <comkieffer.me@gmail.com>
2011-03-28 18:11:14 +0200

    updated get relay state

--- Controller Lib/Board.cpp
+++ Controller Lib/Board.cpp
@@ -2,6 +2,8 @@
 #include <cstdio>
 #include <iostream>
 
+#include <lua.hpp>
+
 #include "Board.h"
 
 //TODO : pass board serial port in ctor
@@ -9,12 +11,13 @@
 
 using namespace std;
 
+extern lua_State* L;
+
 /** Constructor.
   */
 Board::Board()
     : _connected (false)
 {
-
     try {
 #ifdef DEBUG_NO_SERIAL
         fakeInit();
@@ -62,6 +65,53 @@ void Board::readBoardData()
     //Implement This
 }
 
+/** Toggle a relay's state
+  * \param relay The id of the relay we want to toggle
+  * \return true if all went well. If an error occurs (what error ?) the function doesn't exit but
+  *			throws an exception.
+  */
+//TODO : testme
+bool Board::toggleRelay(int relay)
+{
+	//do something on the serial
+	_relay_state[relay - 1] = (_relay_state[relay - 1])? 0 : 1;
+
+	//if something went wrong throw error
+#ifdef SHELL_MODE
+	lua_RelayState();
+#endif
+	return true;
+}
+
+//TODO : this message v
+/** Toggle a relay's state
+  * \param relay The id of the relay we want to toggle
+  * \return true if all went well. If an error occurs (what error ?) the function doesn't exit but
+  *			throws an exception.
+  */
+//TODO : testme
+bool Board::commandRelay(int relay, int action)
+{
+	if (action == RELAY_STATE::ON)
+	{
+		//do something
+		_relay_state[relay - 1] = 1;
+	}
+	else if (action == RELAY_STATE::OFF)
+	{
+		//do something else
+		_relay_state[relay - 1] = 0;
+	}
+	else
+		throw ("Not a valid action.");
+
+#ifdef SHELL_MODE
+	lua_RelayState();
+	cout << "I commanded this bitch to do what you wanted dude !" << endl;
+#endif
+
+	return true;
+}
 /***************************************************************************************************
  **         End Memeber Functions -- Start getters
  **************************************************************************************************/
@@ -97,15 +147,50 @@ int Board::getNumberOfRelays()
 {
     return _relays;
 }
+
+//string Board::getRelayState()
+//{
+//    return "0011001000"; //TODO : this
+//}
+
 int Board::getRelayState(int relay)
 {
-    return 0; //TODO : this
+	return (_relay_state.c_str()[relay - 1] - 48);
 }
 
-string Board::getRelayState()
+#ifdef SHELL_MODE
+//NOTE : I really don't think that I need this
+//Relay State should be updated after each action on the relays.
+void Board::lua_RelayState()
 {
-    return "0000000000"; //TODO : this
+	lua_newtable(L);
+
+	for (int i = 1 ; i < _relays ; i++)
+	{
+	/*
+     * To put values into the table, we first push the index, then the
+     * value, and then call lua_rawset() with the index of the table in the
+     * stack. Let's see why it's -3: In Lua, the value -1 always refers to
+     * the top of the stack. When you create the table with lua_newtable(),
+     * the table gets pushed into the top of the stack. When you push the
+     * index and then the cell value, the stack looks like:
+     *
+     * <- [stack bottom] -- table, index, value [top]
+     *
+     * So the -1 will refer to the cell value, thus -3 is used to refer to
+     * the table itself. Note that lua_rawset() pops the two last elements
+     * of the stack, so that after it has been called, the table is at the
+     * top of the stack.
+     */
+
+		lua_pushnumber(L, i);
+		lua_pushnumber(L, _relay_state[i]);
+		lua_rawset(L, -3);
+	}
+
+	lua_setglobal(L, "relayState");
 }
+#endif
 
 bool Board::isConnected()
 {
@@ -125,4 +210,5 @@ void Board::fakeInit()
     _board_rev = "v0.0";
     _board_program = "";
     _current_ratings = "I probably should decide how this data should be represented";
+    _relay_state ="1001011010";
 }
